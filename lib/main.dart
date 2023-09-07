5c78692d83b8e33e8be01036271cbe3629c2ccaa
elesawy <elesawy928@gmail.com>
2023-09-07 07:57:47 +0300

    conected my to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:chat_app2/views/login_view.dart';
 import 'package:chat_app2/views/register_view.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(List<String> args) {
-  return runApp(const ChatApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ChatApp());
 }
 
 class ChatApp extends StatelessWidget {
