492f45a9885a56a4b7ca4f1cb5a0dad9dc93416a
elesawy <elesawy928@gmail.com>
2023-09-07 08:57:48 +0300

    create Register cubit and added to Register view

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:chat_app2/views/chat_view.dart';
 import 'package:chat_app2/views/login_view.dart';
 import 'package:chat_app2/views/register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'cubits/register_cubit/register_cubit.dart';
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -17,13 +20,21 @@ class ChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        LoginView.id: (context) => const LoginView(),
-        RegisterView.id: (context) =>  RegisterView()
-      },
-      debugShowCheckedModeBanner: false,
-      initialRoute: LoginView.id,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => RegisterCubit(),
+        )
+      ],
+      child: MaterialApp(
+        routes: {
+          LoginView.id: (context) => const LoginView(),
+          RegisterView.id: (context) => RegisterView(),
+          ChatView.id: (context) => const ChatView(),
+        },
+        debugShowCheckedModeBanner: false,
+        initialRoute: LoginView.id,
+      ),
     );
   }
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:chat_app2/cubits/register_cubit/register_cubit.dart';
+import 'package:chat_app2/views/chat_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../widgets/custom_text_form_field.dart';
 
@@ -9,127 +12,134 @@ class RegisterView extends StatelessWidget {
 
   String? email, password;
   GlobalKey<FormState> formKey = GlobalKey();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Color(0xffFAE8E0), Color(0xffD8A7B1)],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: SingleChildScrollView(
-            child: Form(
-              key: formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 120,
-                  ),
-                  Image.asset('assets/images/scholar.png'),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+    return BlocListener<RegisterCubit, RegisterState>(
+      listener: (context, state) {
+        if (state is RegisterLoading) {
+          isLoading = true;
+        } else if (state is RegisterSuccess) {
+          Navigator.pushNamed(context, ChatView.id);
+          isLoading = false;
+        } else if (state is RegisterFailure) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(state.errMessage)));
+          isLoading = false;
+        }
+      },
+      child: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Scaffold(
+          body: Container(
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0xffFAE8E0), Color(0xffD8A7B1)],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        'Welcome To',
-                        style:
-                            TextStyle(fontSize: 24, color: Color(0xff887BB0)),
+                      const SizedBox(
+                        height: 120,
                       ),
-                      Text(
-                        ' Scholar Chat',
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: 'Kanit',
-                            color: Color(0xff887BB0)),
+                      Image.asset('assets/images/scholar.png'),
+                      const SizedBox(
+                        height: 60,
                       ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  CustomTextFormField(
-                    hintText: 'email',
-                    icon: Icons.mail,
-                    onChanged: (value) {
-                      email = value;
-                    },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  CustomTextFormField(
-                    hintText: 'password',
-                    icon: Icons.lock,
-                    onChanged: (value) {
-                      password = value;
-                    },
-                  ),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  GestureDetector(
-                    onTap: () async {
-                      if (formKey.currentState!.validate()) {
-                        try {
-                          final credential = await FirebaseAuth.instance
-                              .createUserWithEmailAndPassword(
-                            email: email!,
-                            password: password!,
-                          );
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'weak-password') {
-                            print('The password provided is too weak.');
-                          } else if (e.code == 'email-already-in-use') {
-                            print('The account already exists for that email.');
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Welcome To',
+                            style: TextStyle(
+                                fontSize: 24, color: Color(0xff887BB0)),
+                          ),
+                          Text(
+                            ' Scholar Chat',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontFamily: 'Kanit',
+                                color: Color(0xff887BB0)),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      CustomTextFormField(
+                        hintText: 'email',
+                        icon: Icons.mail,
+                        onChanged: (value) {
+                          email = value;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomTextFormField(
+                        hintText: 'password',
+                        icon: Icons.lock,
+                        onChanged: (value) {
+                          password = value;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          if (formKey.currentState!.validate()) {
+                            BlocProvider.of<RegisterCubit>(context)
+                                .registerUser(
+                                    email: email!, password: password!);
                           }
-                        } catch (e) {
-                          print(e);
-                        }
-                      }
-                    },
-                    child: Container(
-                      height: 50,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          color: const Color(0xff887BB0),
-                          borderRadius: BorderRadius.circular(16)),
-                      child: const Center(
-                        child: Text(
-                          'Register',
-                          style: TextStyle(fontSize: 24, fontFamily: 'Kanit'),
+                        },
+                        child: Container(
+                          height: 50,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: const Color(0xff887BB0),
+                              borderRadius: BorderRadius.circular(16)),
+                          child: const Center(
+                            child: Text(
+                              'Register',
+                              style:
+                                  TextStyle(fontSize: 24, fontFamily: 'Kanit'),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'have an acount..   ',
-                        style: TextStyle(fontSize: 16),
+                      const SizedBox(
+                        height: 8,
                       ),
-                      Text(
-                        'Login',
-                        style: TextStyle(
-                            fontFamily: 'Kanit',
-                            color: Color(0xff887BB0),
-                            fontSize: 18),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'have an acount..   ',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Text(
+                            'Login',
+                            style: TextStyle(
+                                fontFamily: 'Kanit',
+                                color: Color(0xff887BB0),
+                                fontSize: 18),
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
             ),
           ),
